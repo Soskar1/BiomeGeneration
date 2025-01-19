5442489bf1cc0f301e8d9818cda1535ae80102c3
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2025-01-19 13:38:24 +0200

    Added async and render optimization

--- Assets/Scripts/TerrainRenderer.cs
+++ Assets/Scripts/TerrainRenderer.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections;
 
 namespace Core.BiomeGeneration
 {
@@ -17,16 +19,21 @@ namespace Core.BiomeGeneration
         private void Awake() => m_terrainGeneration = new TerrainGeneration(m_chunkSize, m_chunkHeight, m_noiseData, m_regions);
 
         [ContextMenu("Create Terrain")]
-        public void CreateTerrain()
+        public async void CreateTerrain()
         {
             Vector3Int worldPosition = new Vector3Int((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
-            List<ChunkData> chunkDatas = m_terrainGeneration.GenerateTerrain(worldPosition, m_renderDistance);
+            List<ChunkData> chunkDatas = await Task.Run(() => m_terrainGeneration.GenerateTerrain(worldPosition, m_renderDistance));
+            StartCoroutine(RenderChunks(chunkDatas));
+        }
 
+        private IEnumerator RenderChunks(List<ChunkData> chunkDatas)
+        {
             foreach (ChunkData chunkData in chunkDatas)
             {
                 ChunkRenderer chunkInstance = Instantiate(m_chunkPrefab, chunkData.WorldPosition, Quaternion.identity);
                 chunkInstance.transform.parent = transform;
                 chunkInstance.Render(chunkData);
+                yield return new WaitForEndOfFrame();
             }
         }
     }
